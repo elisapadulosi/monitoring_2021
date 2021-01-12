ba7d490b2245e47eb3bfc55881c79763f39baeba
elisapadulosi <73693976+elisapadulosi@users.noreply.github.com>
2021-01-12 21:00:24 +0100

    Update R_code_exam_simulation.r

--- R_code_exam_simulation.r
+++ R_code_exam_simulation.r
@@ -5,7 +5,7 @@ library(ncdf4)
 setwd("/Users/elisapadulosi/Google Drive/LAB/") #to set the working directory
 library(raster)
 
-tjan <- raster("c_gls_LST10-DC_202101010000_GLOBE_GEO_V1.2.1.nc") # temperature data from Copernicus
+tjan <- raster("c_gls_LST10-DC_202101010000_GLOBE_GEO_V1.2.1.nc") # temperature data in january from Copernicus
 
 plot(tjan)
 cltjan <- colorRampPalette(c('black','grey','light grey'))(100)# colors
@@ -14,6 +14,7 @@ cltjan <- colorRampPalette(c('blue','yellow','red'))(100)
 plot(tjan, col=cltjan) # to change the colors
 
 #difference between the temperature of january and october at the soil level # higher the difference->more red the image
+toct<-raster("c_gls_LST10-DC_202010010000_GLOBE_GEO_V1.2.1.nc")#name of the file that contains the temperature data of October
 dift <- tjan - toct
 cldif <- colorRampPalette(c('blue','white','red'))(100) #
 plot(dift, col=cldif) # if it gives an error it's because you have to download the january data from Copernicus also
