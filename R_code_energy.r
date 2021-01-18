6d7752678fa2596edc541a948a433bf6d9d02f70
elisapadulosi <73693976+elisapadulosi@users.noreply.github.com>
2021-01-18 10:54:40 +0100

    Update R_code_energy.r

--- R_code_energy.r
+++ R_code_energy.r
@@ -41,13 +41,80 @@ plot(copNDVI_Italy, col=clymax)
 
 
 ## 18/01/2021
-# deforestation
+# DEFORESTATION
 library(raster)
 library(RStoolbox)
 
+setwd("/Users/elisapadulosi/Google Drive/LAB/") #to set the working directory
+defor1 <- brick("defor1_.jpg")
+defor1 #to have information about the file
 
+plotRGB(defor1, 1, 2, 3, stretch="Lin") #123 because it's an red g(yellow) blue system
+
+
+defor2 <- brick("defor2_.jpg")
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+
+#put the 2 images near one another
+par(mfrow=c(2,1))
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+
+#calculate the vegetation indices
+# dvi for the first period
+dvi1 <- defor1$defor1_.1 - defor1$defor1_.2
+dev.off()
+plot(dvi1)
+#change the colors
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
+plot(dvi1, col=cl)
+
+#dvi2
+dvi2 <- defor2$defor2_.1 - defor2$defor2_.2
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
+plot(dvi2, col=cl)
+
+#2 dvi together
+par(mfrow=c(2,1))
+plot(dvi1, col=cl)
+plot(dvi2, col=cl)
+
+par(mfrow=c(2,1))
+plot(dvi1, col=cl, main="DVI before cut")
+plot(dvi2, col=cl, main="DVI after cut")
+
+#difference biomass before cut and after cut
+difdvi <- dvi1 -dvi2
+dev.off()
+plot(difdvi)
+#colors (amount of biomass lost during the cut)
+cldif <- colorRampPalette(c('blue','white','red'))(100) 
+plot(difdvi, col=cldif) #amount of energy lost because of the loss of biomass
+
+#title of teh graph
+cldif <- colorRampPalette(c('blue','white','red'))(100) 
+plot(difdvi, col=cldif, main="amount of energy lost!") 
+
+#histogram
+hist(difdvi, col="red")
+
+#####final analysis
+#all these things together
+#defor1
+#defor2
+#dvi1
+#dvi2
+#difdvi
+
+par(mfrow=c(3,2))
+plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+plot(dvi1, col=cl, main="biomass before cut")
+plot(dvi2, col=cl, main="biomass after cut")
+plot(difdvi, col=cldif, main="amount of energy lost!")
 
 
 
 
  
+
