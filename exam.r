113d024bae12869e2eda98c9a5595b7cd789ef5a
elisapadulosi <73693976+elisapadulosi@users.noreply.github.com>
2021-02-04 00:58:59 +0100

    Update exam.r

--- exam.r
+++ exam.r
@@ -1,16 +1,14 @@
 ## Australia bushfire season (June 2019-May 2020)
 
 # needed packages
-insall.packages("raster")
+
+install.packages("raster")
 library(raster)
 
-install.packages("rgdal")
-library(rgdal)
-
-insall.packages("ncdf4")
+install.packages("ncdf4")
 library(ncdf4)
 
-insall.packages("RStoolbox")
+install.packages("RStoolbox")
 library(RStoolbox)
 
 install.packages("ggplot2")
@@ -30,37 +28,41 @@ ndviafter<-raster("c_gls_NDVI300_202006010000_GLOBE_PROBAV_V1.0.1.nc")
 ndviduring<-raster("c_gls_NDVI300_202002210000_GLOBE_PROBAV_V1.0.1.nc")
 
 #let's crop the ndvibefore (the global one is too heavy to plot). I want to study only Australia, so I must find its spatial coordinates
-ext<-c(12,42,105,162)
+ext<-c(110,155,-45,-10)#xmin xmax, ymin e ymax
 ndvibefore<-crop(ndvibefore, ext)
 
 #let's crop the ndviafter
-ext<-c(12,42,105,162)
 ndviafter<-crop(ndviafter, ext)
 
 #let's crop ndviduring
-ext<-c(12,42,105,162)
 ndviduringr<-crop(ndviduring, ext)
 
-#let's plot and change the colors to the plotted images
+#let's plot and change the colors to the plotted images with the function colorRampPalette
+#function main to name the graphs
 #before
 cl <- colorRampPalette(c('blue','red','yellow'))(100)
-plot(ndvibefore)
+plot(ndvibefore, col=cl,main="NDVI before bushfires")
 
 #during
-cl <- colorRampPalette(c('blue','red','yellow'))(100)
-plot(ndviafter)
+plot(ndviduring, col=cl,main="NDVI during bushfires")
 
 #after
-cl <- colorRampPalette(c('blue','red','yellow'))(100)
-plot(ndviduring)
+plot(ndviafter, col=cl,main="NDVI after bushfires")
 
 # let's see the differences before and after
 difbushfires<- ndvibefore-ndviafter
-plot(difbushfire, col= cl)
+plot(difbushfires, col= cl) #south and east parts have decrease of ndvi, conistent with the drescrese in the vegetation due to the bushfires
 
 #let's create a grapf with: ndvibefore, ndviduring, ndviafter, difbushfires
 # par function to have multiple graphs in a single plot 
+#used main to namw the table
 par(mfrow=c(1,4)) # 1 row, 4 colums
+plot(ndvibefore, col=cl,main="NDVI before bushfires")
+plot(ndviduring, col=cl,main="NDVI during bushfires")
+plot(ndviafter, col=cl,main="NDVI after bushfires")
+plot(difbushfires, col= cl,main="NDVI differnce")
+
+
 
 
 
