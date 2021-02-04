4ed9bf55e7611ede058a2737880e21428a31578b
elisapadulosi <73693976+elisapadulosi@users.noreply.github.com>
2021-02-04 16:24:11 +0100

    Update exam.r

--- exam.r
+++ exam.r
@@ -5,7 +5,7 @@
 install.packages("raster")
 library(raster)
 
-# used to read Copernicis data
+# used to read Copernicus data
 install.packages("ncdf4")
 library(ncdf4)
 
@@ -21,21 +21,24 @@ library(ggplot2)
 setwd("/Users/elisapadulosi/Google Drive/LAB/")
 
 # for this project I want to study NDVI, fires disturbance and land surface temperature. See if and how those variables have changed over time due to the Australia bushfire season
-# I choose to plot also the fires disturbance to see the  differences in occurrence from other fires in Australia (in which they are very common)
+# I choose to plot also the fires disturbance to see the  differences in occurrence from this period to others in Australia (in which they are very common)
 # My hypothesis: NDVI before > NDVIafter  Tbefore < Tafter
 
 
 # NDVI is used to observe if the vegetation is alive
-# ndvi before 10/04/2019
+# ndvi before Australia bushfire season (June 2019-May 2020) -> 10/04/2019
 ndvibefore<-raster("c_gls_NDVI300_201904010000_GLOBE_PROBAV_V1.0.1.nc")
 
-# ndvi after 10/06/2020
+# ndvi after Australia bushfire season (June 2019-May 2020) -> 10/06/2020
 ndviafter<-raster("c_gls_NDVI300_202006010000_GLOBE_PROBAV_V1.0.1.nc")
 
-# ndvi during the bushfires 29/02/2020
+# ndvi during the bushfires Australia bushfire season (June 2019-May 2020) -> 29/02/2020
 ndviduring<-raster("c_gls_NDVI300_202002210000_GLOBE_PROBAV_V1.0.1.nc")
 
-#let's crop the ndvibefore (the global one is too heavy to plot). I want to study only Australia, so I must find its spatial coordinates
+ 
+# I want to study only Australia, so I must find its spatial coordinates
+# I choose to study all the continent to see the widespread consequences
+# let's crop the ndvibefore (the global one is too heavy to plot). 
 ext<-c(110,155,-45,-10)#xmin xmax, ymin e ymax
 ndvibefore<-crop(ndvibefore, ext)
 
@@ -72,13 +75,13 @@ plot(difbushfires, col= Ncl,main="NDVI differnce")
 
 
 # TEMPERATURE (10 days-> to let the athmosphere react)
-# temperature before 1-10/04/2019
+# temperature before Australia bushfire season (June 2019-May 2020) -> 1-10/04/2019
 tbefore<- raster("c_gls_LST10-DC_201904010000_GLOBE_GEO_V1.2.1.nc")
 
-#temperature during 21-29/02/2020
+#temperature during Australia bushfire season (June 2019-May 2020) -> 21-29/02/2020
 tduring<-raster("c_gls_LST10-DC_202002210000_GLOBE_GEO_V1.2.1.nc")
 
-#temperature after 1-10/06/2020
+#temperature after Australia bushfire season (June 2019-May 2020)-> 1-10/06/2020
 tafter<- raster("c_gls_LST10-DC_202006010000_GLOBE_GEO_V1.2.1.nc")
 
 #let's crop because we gave global data, but we are interested only in Australia
@@ -92,7 +95,7 @@ tduring<-crop(tduring, ext)
 #temperature after
 tafter<-crop(tafter, ext)
 
-#let's plot one t to see if the cooridates are correct
+#let's plot one T to see if the cooridates are correct
 plot(tafter)
 # the coordinates are correct!!
 
@@ -115,7 +118,7 @@ plot(tdifbushfires, col= tcl, main="difference T before and after") # higher the
 
 #let's create a grapf with: tbefore, tduring, tafter, tdifbushfires
 # par function to have multiple graphs in a single plot 
-#used main to namw the table
+#used main to name the table
 par(mfrow=c(1,4)) # 1 row, 4 colums
 plot(tbefore, col=tcl,main="T before bushfires")
 plot(tduring, col=tcl,main="T during bushfires")
@@ -125,14 +128,15 @@ plot(tdifbushfires, col= tcl, main="difference T before and after")
 
 
 # BUSHFIRES
-# bushfires before 1-10/04/2019
-beforefires<-raster("c_gls_BA300_201904100000_GLOBE_PROBAV_V1.1.1.nc")
+# I took also the fires before and after that period to see if there were the same fires in the past
+# bushfires before Australia bushfire season (June 2019-May 2020)-> 21-30/04/2015
+beforefires<-raster("c_gls_BA300_201504300000_GLOBE_PROBAV_V1.0.1.nc")
 
-# bushfires during 21-29/02/2020
+# bushfires during Australia bushfire season (June 2019-May 2020) -> 21-29/02/2020
 duringfires<-raster("c_gls_BA300_202002290000_GLOBE_PROBAV_V1.1.1.nc")
 
-# bushfires after 1-10/06/2020
-afterfires<-raster("c_gls_BA300_202006100000_GLOBE_PROBAV_V1.1.1.nc	")
+# bushfires after Australia bushfire season (June 2019-May 2020) -> 1-10/06/2021
+afterfires<-raster("c_gls_BA300_202101100000_GLOBE_PROBAV_V1.1.1.nc")
 
 #let's crop on Australia
 #before
