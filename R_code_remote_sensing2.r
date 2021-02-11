06f70f5f941def0f8cb568af35c1b39e613f8a44
elisapadulosi <73693976+elisapadulosi@users.noreply.github.com>
2021-02-11 22:42:11 +0100

    Update R_code_remote_sensing2.r

--- R_code_remote_sensing2.r
+++ R_code_remote_sensing2.r
@@ -9,16 +9,19 @@ p224r63_2011 <- brick("p224r63_2011_masked.grd") #brick function make possible t
 p224r63_2011 #to have information about one of the seven layers
 plot(p224r63_2011)
 
-cl <- colorRampPalette(c('black','grey','light grey'))(100) # change colorsù
+cl <- colorRampPalette(c('black','grey','light grey'))(100) # change colors
 plot(p224r63_2011, col=cl)
 
+# to see 4 bands
 par(mfrow=c(2,2))
 clb <- colorRampPalette(c('dark blue','blue','light blue'))(100) # 
-plot(p224r63_2011$B1_sre, col=clb)
+plot(p224r63_2011$B1_sre, col=clb)# $ to link in R
 
-clg <- colorRampPalette(c('dark green','green','light green'))(100) # 
+# to change the color of the second band
+clg <- colorRampPalette(c('dark green','green','light green'))(100) 
 plot(p224r63_2011$B2_sre, col=clg)
 
+# to change the color of the third band
 clr <- colorRampPalette(c('dark red','red','pink'))(100) # 
 plot(p224r63_2011$B3_sre, col=clr)
 
